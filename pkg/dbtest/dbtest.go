c2109937e32b5f0783a35b2dbc3d807917416823
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2023-08-03 17:59:12 +0600

    Create dbtest.go

---
+++ pkg/dbtest/dbtest.go
@@ -0,0 +1,23 @@
+package dbtest
+
+import (
+	"github.com/khulnasoft-labs/vul-java-db/pkg/types"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/khulnasoft-labs/vul-java-db/pkg/db"
+)
+
+func InitDB(t *testing.T, indexes []types.Index) (db.DB, error) {
+	tmpDir := t.TempDir()
+	dbc, err := db.New(tmpDir)
+	require.NoError(t, err)
+
+	err = dbc.Init()
+	require.NoError(t, err)
+
+	err = dbc.InsertIndexes(indexes)
+	require.NoError(t, err)
+	return dbc, nil
+}
